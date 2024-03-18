9423e68a5db833ea59f649fdf1a4a828b655ec39
bruninnn11 <163931405+bruninnn11@users.noreply.github.com>
2024-03-18 19:12:30 -0300

    Add files via upload

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,15 @@
+
+package javaapplication3;
+
+import java.util.Scanner;
+
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        Scanner n = new Scanner (System.in);
+        float numero1 = n.nextFloat();
+        System.out.println(numero1);
+    }
+    
+}
